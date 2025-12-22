7ccbdab20fd41ac712d147d6f628f52c399c46a3
KhalimahMusaadah <khalimahmusaadah@gmail.com>
2025-12-22 14:49:55 +0700

    first commit

---
+++ .history/application/controllers/api/Wrapping_20251222143821.php
@@ -0,0 +1,2 @@
+<?php
+defineD('B')
---
+++ .history/application/controllers/api/Wrapping_20251222144604.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Wrapping extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+        header('Content-Type: application/json');
+    }
+
+    /* POST api/wrapping/process */
+    public function process(){
+        echo json_encode([
+            'status' => 'OK'
+        ])
+    }
+}
